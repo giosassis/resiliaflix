ebf6c00c901c3f2fc5dbbb380e2eed17b9eb249b
Marcos Paulo M Cabral <paulo_kpv@hotmail.com>
2021-05-17 01:47:33 -0300

    ajuste de pastas

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-let $ = document.querySelector.bind(document);
-let buttonLogin = $("#btnLogin");
-let email = $("#email");
-let senha = $("#Senha");
+$(window).scroll(function () {//mudar a navbar ao scrollar
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > 100);
+})
+let buttonLogin = document.getElementById("btnLogin");
+let senha = document.getElementById("Senha");
+let email = document.getElementById("email");
 
 function vEmail(email) {
 	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -13,10 +15,7 @@ buttonLogin.addEventListener("click", (event) => {
 	if (!vEmail(email.value) && senha.value.length < 4) alert("email e Senha errados");
     else if(!vEmail(email.value) && senha.value.length>3) alert("Email errado");
     else if(vEmail(email.value) && senha.value.length<4) alert("Senha errada");
-    else window.open("../index.html", "_self");
+    else window.open("index.html", "_self");
 	console.log(senha.values)	
 	
 });
-$(window).scroll(function () {//mudar a navbar ao scrollar
-    $('nav').toggleClass('scrolled', $(this).scrollTop() > 100);
-})
